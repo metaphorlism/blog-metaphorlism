9675b7394ea50ec8025b0fe19c3cc1b01685ae12
LimTangmeng <limtangmeng100@gmail.com>
2023-04-18 21:41:28 +0700

    Add Blogs page

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,16 +1,37 @@
-import { marked } from "marked";
+import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
 import { getBlog } from "@/config/axios";
+import Image from "next/image";
 
 export default async function Page({ params }: { params: { id: number } }) {
-  // const blog = await getBlog(1).then((text) => {marked(text)});
-  const blog = await getBlog(params.id);
+  const { metadata, content } = await getBlog(params.id).then((text) => {
+    const matterResult = matter(text);
+
+    return {
+      metadata: {
+        title: matterResult.data.title || "",
+        description: matterResult.data.description,
+        thumbnail: matterResult.data.thumbnail || "",
+      },
+      content: matterResult.content,
+    };
+  });
 
   return (
-    <main className="w-15 mx-auto">
-      {/* <article
-        dangerouslySetInnerHTML={{ __html: blog }}
-        className="prose xl:prose-xl mx-auto"
-      ></article> */}
+    <main className="relative w-[40ch] md:w-[60ch] lg:w-[80ch] mx-auto flex flex-col mt-5">
+      <h1 className="text-3xl font-bold">{metadata.title}</h1>
+      {metadata.thumbnail ? (
+        <Image
+          src={metadata.thumbnail}
+          width={1920}
+          height={1080}
+          alt={metadata.title}
+          className="my-5 shadow-xl"
+        ></Image>
+      ) : null}
+      <article className="prose lg:prose-xl">
+        <Markdown>{content}</Markdown>
+      </article>
     </main>
   );
 }
--- src/config/axios.ts
+++ src/config/axios.ts
@@ -8,10 +8,18 @@ async function fetchData(props: AxiosRequestConfig) {
   });
 }
 
+// export async function getBlog(id: number) {
+//   return fetchData({ url: `/todos/${id}` }).then(
+//     (res: AxiosResponse<{ title: string; id: number }>) => {
+//       return res.data;
+//     }
+//   );
+// }
+
 export async function getBlog(id: number) {
-  return fetchData({ url: `/todos/${id}` }).then(
-    (res: AxiosResponse<{ title: string; id: number }>) => {
-      return res.data;
-    }
-  );
+  return fetchData({
+    url: `/ds2egk1ut/raw/upload/v1681706923/blog_markdowns/154f3360-4b58-4610-98c8-c77b97646fa9.md`,
+  }).then((res: AxiosResponse<string>) => {
+    return res.data;
+  });
 }
